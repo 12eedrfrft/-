b9abe786263a361bec3cfbdd39de1c493764bb29
12eedrfrft <w433358@gmail.com>
2025-06-03 20:48:03 +0800

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,22 @@
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #000;
+  margin: 0;
+}
+
+#game {
+  border: 2px solid #fff;
+  background: #111;
+}
+
+#score {
+  position: absolute;
+  top: 20px;
+  left: 20px;
+  color: #fff;
+  font-family: Arial, sans-serif;
+  font-size: 20px;
+}
